openwrt/openwrt

commit a717428828558577760abfcf83a54e3d7459ee0b

treewide: use new procd sysupgrade $UPGRADE_BACKUP variable
It's a variable set by procd that should replace hardcoded
/tmp/sysupgrade.tgz.

This change requires the most recent procd with the commit 0f3c136
("sysupgrade: set UPGRADE_BACKUP env variable").

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
(cherry picked from commit 641f6b6c26cb9ab5e1198810015e5f4b2b5b34ad)

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -65,7 +65,7 @@ platform_do_upgrade_combined() {
 	then
 		local rootfspart=$(platform_find_rootfspart "$partitions" "$kernelpart")
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$UPGRADE_BACKUP" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $UPGRADE_BACKUP"
 
 		if [ "$PLATFORM_DO_UPGRADE_COMBINED_SEPARATE_MTD" -ne 1 ]; then
 		    ( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
@@ -164,7 +164,7 @@ platform_do_upgrade_compex() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$UPGRADE_BACKUP" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $UPGRADE_BACKUPs"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \

target/linux/ath25/base-files/lib/upgrade/platform.sh
@@ -67,7 +67,7 @@ platform_do_upgrade() {
 	   [ ${erase_size:-0} -gt 0 ];
 	then
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$UPGRADE_BACKUP" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $UPGRADE_BACKUP"
 
 		( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
 		  dd if="$1" bs=$CI_BLKSZ skip=$((1+$kern_blocks)) count=$root_blocks 2>/dev/null ) | \

target/linux/ath79/base-files/lib/upgrade/platform.sh
@@ -14,7 +14,7 @@ redboot_fis_do_upgrade() {
 	if [ "$magic" = "4349" ]; then
 		local kern_length=0x$(dd if="$sysup_file" bs=2 skip=1 count=4 2>/dev/null)
 
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$UPGRADE_BACKUP" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $UPGRADE_BACKUP"
 		dd if="$sysup_file" bs=64k skip=1 2>/dev/null | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 
@@ -22,7 +22,7 @@ redboot_fis_do_upgrade() {
 		local board_dir=$(tar tf $sysup_file | grep -m 1 '^sysupgrade-.*/$')
 		local kern_length=$(tar xf $sysup_file ${board_dir}kernel -O | wc -c)
 
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$UPGRADE_BACKUP" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $UPGRADE_BACKUP"
 		tar xf $sysup_file ${board_dir}kernel ${board_dir}root -O | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 

target/linux/brcm2708/base-files/lib/upgrade/platform.sh
@@ -91,8 +91,8 @@ platform_copy_config() {
 	if export_partdevice partdev 1; then
 		mkdir -p /boot
 		[ -f /boot/kernel.img ] || mount -t vfat -o rw,noatime "/dev/$partdev" /boot
-		cp -af "$CONF_TAR" "/boot/$BACKUP_FILE"
-		tar -C / -zxvf "$CONF_TAR" boot/config.txt
+		cp -af "$UPGRADE_BACKUP" "/boot/$BACKUP_FILE"
+		tar -C / -zxvf "$UPGRADE_BACKUP" boot/config.txt
 		sync
 		unmount /boot
 	fi

target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -17,7 +17,7 @@ enable_image_metadata_check
 
 apalis_copy_config() {
 	apalis_mount_boot
-	cp -af "$CONF_TAR" "/boot/$BACKUP_FILE"
+	cp -af "$UPGRADE_BACKUP" "/boot/$BACKUP_FILE"
 	sync
 	umount /boot
 }

target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -38,7 +38,7 @@ zyxel_do_upgrade() {
 	tar Oxf $tar_file ${board_dir}/kernel | mtd write - kernel
 
 	if [ "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ]; then
-		tar Oxf $tar_file ${board_dir}/root | mtd -j "$CONF_TAR" write - rootfs
+		tar Oxf $tar_file ${board_dir}/root | mtd -j "$UPGRADE_BACKUP" write - rootfs
 	else
 		tar Oxf $tar_file ${board_dir}/root | mtd write - rootfs
 	fi

target/linux/mediatek/base-files/lib/upgrade/platform.sh
@@ -54,7 +54,7 @@ platform_check_image() {
 platform_copy_config_emmc() {
 	mkdir -p /recovery
 	mount -o rw,noatime /dev/mmcblk0p1 /recovery
-	cp -af "$CONF_TAR" "/recovery/$BACKUP_FILE"
+	cp -af "$UPGRADE_BACKUP" "/recovery/$BACKUP_FILE"
 	sync
 	umount /recovery
 }

target/linux/octeon/base-files/lib/upgrade/platform.sh
@@ -25,7 +25,7 @@ platform_copy_config() {
 	case "$(board_name)" in
 	erlite)
 		mount -t vfat /dev/sda1 /mnt
-		cp -af "$CONF_TAR" "/mnt/$BACKUP_FILE"
+		cp -af "$UPGRADE_BACKUP" "/mnt/$BACKUP_FILE"
 		umount /mnt
 		;;
 	esac

SerebryakovS/Fau2OS

commit 5068613373c282965f27a6cefbbd29a8b71e35e6

ar71xx: fix typo in platform_do_upgrade_compex()
Fixes: a71742882855 ("treewide: use new procd sysupgrade $UPGRADE_BACKUP variable")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
(cherry picked from commit 63d611390c4d34a838e744e278529f30f9d2cc20)

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -164,7 +164,7 @@ platform_do_upgrade_compex() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$UPGRADE_BACKUP" ] && append="-j $UPGRADE_BACKUPs"
+		[ -f "$UPGRADE_BACKUP" ] && append="-j $UPGRADE_BACKUP"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \