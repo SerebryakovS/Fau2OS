openwrt/openwrt

commit d640cbac0e76e3638504cb6b239886e8ff75018f

mediatek: mt7622: don't rely on existing image for sysupgrade
Don't reply on mapped rootfs partition but rather just take what ever
has been set to the kernel cmdline root= parameter as a hint to decide
which media to install sysupgrade to on the BananaPi BPi-R64.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
@@ -6,8 +6,8 @@ platform_do_upgrade() {
 
 	case "$board" in
 	bananapi,bpi-r64)
-		export_bootdevice
-		export_partdevice rootdev 0
+		local rootdev="$(cmdline_get_var root)"
+		rootdev="${rootdev##*/}"
 		case "$rootdev" in
 		mmc*)
 			CI_ROOTDEV="$rootdev"

SerebryakovS/Fau2OS

commit 7e2d65aa5e9556b9346aad24e7aaf9578b05d831

mediatek: mt7622: fix sysupgrade on MMC on BPi-R64
A previous attempt to simplify things went wrong and now sysupgrade
is broken on this device. Fix that.

Fixes: d640cbac0e ("mediatek: mt7622: don't rely on existing image for sysupgrade")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
@@ -8,6 +8,7 @@ platform_do_upgrade() {
 	bananapi,bpi-r64)
 		local rootdev="$(cmdline_get_var root)"
 		rootdev="${rootdev##*/}"
+		rootdev="${rootdev%p[0-9]*}"
 		case "$rootdev" in
 		mmc*)
 			CI_ROOTDEV="$rootdev"